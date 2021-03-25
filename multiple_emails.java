f86fa4f74e8e8d8e147eac2e807b33a9218c15bc
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-03-25 23:58:30 +0530

    Email to multiple by changing name
    Practiced strings by doing it.

---
+++ multiple_emails.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class multiple_emails{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the name of person whom you want to send the mail : ");
+        String nameofperson = sc.nextLine();
+        String mail = "Dear <|name|>, Thanks a lot!";
+        mail = mail.replace("<|name|>",+nameofperson);
+        System.out.println(mail);
+        sc.close();
+    }
+}
