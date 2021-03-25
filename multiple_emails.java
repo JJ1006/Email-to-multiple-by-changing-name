ba5a09a822c462c6408e0717c63ec482c0d41183
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-03-26 00:11:13 +0530

    We can send the same mail to multiple people!!!
    Doing practice of strings.

--- multiple_emails.java
+++ multiple_emails.java
@@ -6,7 +6,7 @@ public class multiple_emails{
         System.out.println("Please enter the name of person whom you want to send the mail : ");
         String nameofperson = sc.nextLine();
         String mail = "Dear <|name|>, Thanks a lot!";
-        mail = mail.replace("<|name|>",+nameofperson);
+        mail = mail.replace("<|name|>",nameofperson);
         System.out.println(mail);
         sc.close();
     }
